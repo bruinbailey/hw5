140e18a59a2d069edee40eb504e9549e9c34bd89
Greg Bailey <bruinbailey@gmail.com>
2018-11-28 17:52:01 -0500

    got assignment component showing list of assignments

--- frontend/src/app/assignment/assignment.component.ts
+++ frontend/src/app/assignment/assignment.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Observable, of } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
+import { Assignment } from '../models/Assignments';
+import { AssignmentDataService } from '../models/assignment-data.service';
 
 @Component({
   selector: 'app-assignment',
@@ -11,12 +13,18 @@ import { switchMap } from 'rxjs/operators';
 })
 export class AssignmentComponent implements OnInit {
   section: Observable<String>;
+  sectionAssignments: Observable<Assignment[]>;
 
   constructor(
-	private route: ActivatedRoute,
+  private route: ActivatedRoute,
+  private assignmentDataService: AssignmentDataService
   ) { }
 
   ngOnInit() {
+    this.sectionAssignments = this.route.paramMap.pipe(
+      switchMap((params: ParamMap) => this.assignmentDataService.getAssignments(params.get('section')))
+      );
+
 	  // This code gets the :section parameter out of the route
 	  this.section = this.route.paramMap.pipe(
 		switchMap((params: ParamMap) => of(params.get('section')))
